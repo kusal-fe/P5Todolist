94dcf9f4d781226739f11c5279e687b143e5cf20
KusalVf <126545429+kusal-fe@users.noreply.github.com>
2025-07-27 20:15:34 +0530

    Complete Tutorial 2

--- P5Todo/main/views.py
+++ P5Todo/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Hello World!</h1>")
-
-def v1(response):
-    return HttpResponse("<h1>View 1</h1>")
+def index(response, id):
+    ls = TodoList.objects.get(id=id)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, item.text))
