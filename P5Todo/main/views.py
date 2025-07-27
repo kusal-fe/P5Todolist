85793465b80d6d3ea90b7eb128205cd37707e9d8
KusalVf <126545429+kusal-fe@users.noreply.github.com>
2025-07-27 23:34:53 +0530

    Complete Tutorial 4

--- P5Todo/main/views.py
+++ P5Todo/main/views.py
@@ -7,4 +7,7 @@ from .models import TodoList, Item
 def index(response, id):
     ls = TodoList.objects.get(id=id)
     #item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, "item.text"))
+    return render(response, 'main/list.html', {"ls":ls})
+
+def home(response):
+    return render(response, 'main/home.html', {})
