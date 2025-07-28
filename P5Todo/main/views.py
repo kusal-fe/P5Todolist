a3a71384ddbe2ca8049f8ec5f519ab9779afac13
KusalVf <126545429+kusal-fe@users.noreply.github.com>
2025-07-28 13:28:11 +0530

    Complete Tutorial 6

--- P5Todo/main/views.py
+++ P5Todo/main/views.py
@@ -8,6 +8,25 @@ from .forms import CreateNewList
 def index(response, id):
     ls = TodoList.objects.get(id=id)
     #item = ls.item_set.get(id=1)
+
+    if response.method=="POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c"+str(item.id))=="clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("Invalid Input")
+
     return render(response, 'main/list.html', {"ls":ls})
 
 def home(response):
