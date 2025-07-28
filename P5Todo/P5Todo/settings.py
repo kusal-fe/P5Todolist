15381ee23c5498756c04e08adc277261fd41cdb1
KusalVf <126545429+kusal-fe@users.noreply.github.com>
2025-07-28 15:19:16 +0530

    Complete Tutorial 9

--- P5Todo/P5Todo/settings.py
+++ P5Todo/P5Todo/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "main.apps.MainConfig",
+    "register.apps.RegisterConfig",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
